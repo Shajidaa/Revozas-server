e7ee44c0f61bee83bb64d4552b0688528664ce50
Shajidaa <shajidaakterlopa101@gmail.com>
2025-11-25 14:34:20 +0600

    bestseller api add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xodph41.mongodb.net/?appName=Cluster0`;
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello revoza!");
 });
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +30,7 @@ async function run() {
 
     const myDb = client.db("revoza_db");
     const productCollection = myDb.collection("products");
+    const myProductsCollection = myDb.collection("myProducts");
     //product apis
 
     app.get("/products", async (req, res) => {
@@ -51,7 +52,38 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    // /bestSeller-product
+    app.get("/bestSeller-product", async (req, res) => {
+      const result = await productCollection
+        .find({ bestSeller: true })
+        .sort({ createdAt: -1 })
+        .toArray();
 
+      res.send(result);
+    });
+
+    app.get("/product-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    // my product api
+
+    app.get("/create-product", async (req, res) => {
+      const query = {};
+      const cursor = myProductsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.delete("/my-product/:id", async (res, req) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await myProductsCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
